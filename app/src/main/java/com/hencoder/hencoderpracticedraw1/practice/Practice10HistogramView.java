bd16bcdd5d5d86fea036509e8b44ffde52a0dbde
JeremyHwc <jeremy_hwc@163.com>
2017-11-08 11:31:55 +0800

    完成所有练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -2,6 +2,9 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,5 +29,37 @@ public class Practice10HistogramView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        paint.setColor(Color.WHITE);
+        canvas.drawLine(100, 50, 100, 600, paint);
+        canvas.drawLine(100, 600, 1000, 600, paint);
+
+        paint.setColor(Color.GREEN);
+        RectF rect1 = new RectF(120, 580, 170, 600);
+        canvas.drawRect(rect1, paint);
+
+        RectF rect2 = new RectF(180, 560, 230, 600);
+        canvas.drawRect(rect2, paint);
+
+        RectF rect3 = new RectF(240, 580, 290, 600);
+        canvas.drawRect(rect3, paint);
+
+        RectF rect4 = new RectF(300, 300, 350, 600);
+        canvas.drawRect(rect4, paint);
+
+        RectF rect5 = new RectF(360, 190, 410, 600);
+        canvas.drawRect(rect5, paint);
+
+        RectF rect6 = new RectF(420, 200, 470, 600);
+        canvas.drawRect(rect6, paint);
+
+        RectF rect7 = new RectF(480, 480, 530, 600);
+        canvas.drawRect(rect7, paint);
+
+        paint.setTextSize(40);
+        paint.setColor(Color.WHITE);
+        canvas.drawText("直方图",480,650,paint);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -2,22 +2,30 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice11PieChartView extends View {
 
+    private final Context context;
+
     public Practice11PieChartView(Context context) {
         super(context);
+        this.context=context;
     }
 
     public Practice11PieChartView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        this.context=context;
     }
 
     public Practice11PieChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context=context;
     }
 
     @Override
@@ -26,5 +34,28 @@ public class Practice11PieChartView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+
+        Paint paint=new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        RectF oval=new RectF(300,200,800,700);
+        paint.setColor(Color.GRAY);
+        canvas.drawArc(oval,5,10,true,paint);
+
+        paint.setColor(Color.WHITE);
+        canvas.drawArc(oval,20,10,true,paint);
+
+        paint.setColor(Color.GREEN);
+        canvas.drawArc(oval,33,50,true,paint);
+
+        paint.setColor(Color.BLUE);
+        canvas.drawArc(oval,86,94,true,paint);
+
+        paint.setColor(context.getColor(android.R.color.holo_orange_dark));
+        canvas.drawArc(oval,0,-45,true,paint);
+
+
+        paint.setColor(context.getColor(android.R.color.holo_red_dark));
+        RectF oval1=new RectF(280,180,780,680);
+        canvas.drawArc(oval1,-45,-135,true,paint);
     }
 }
